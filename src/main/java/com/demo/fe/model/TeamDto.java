152b623cc9db6c7fa7294acdf05fde41561aebe8
BigDaddyMM <105431068+BigDaddyMM@users.noreply.github.com>
2023-05-09 10:30:18 +0200

    fixed teamentity and dto

--- src/main/java/com/demo/fe/model/TeamDto.java
+++ src/main/java/com/demo/fe/model/TeamDto.java
@@ -2,10 +2,12 @@ package com.demo.fe.model;
 
 import com.demo.fe.data.entity.EmployeeEntity;
 
+import java.util.List;
+
 public class TeamDto {
 
     private String title;
-    private EmployeeEntity employee;
+    private List<EmployeeEntity> employee;
     private double gained;
     private double lost;
     private double profit;
